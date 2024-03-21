3e0dedc861f64c6a60e164ea040163d75a2b4659
PavelOlkhovoi <PavelOlkhovoi@users.noreply.github.com>
2024-03-21 13:27:20 +0000

    deploy: PavelOlkhovoi/github-actions-react@84ee0ca8a2e9e7565a4317618e316011e2f5eaac

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Public demo B with new deploy !
+          Deploy
         </a>
       </header>
     </div>
